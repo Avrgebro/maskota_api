b746e52e68a46a2ff70b1b1848657caae7d7cfb9
Jose Bejarano <jose@caffelli.com>
2022-04-02 11:05:42 -0500

    Added basic auth flow

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Password;
+use App\Traits\ApiResponser;
+use App\Models\User;
+use App\Http\Requests\Auth\LoginRequest;
+
+class AuthController extends Controller
+{
+    use ApiResponser;
+
+    public function login (LoginRequest $request) {
+
+        if(!$request->authenticate()){
+            return $this->error([], 'Incorrect credentials', 401);
+        } else {
+            $user = $request->user();
+
+            $token = $user->createToken('API Token ' . Str::random(10));
+
+            return $this->success([
+                'user' => $user,
+                'token' => $this->token($token->plainTextToken),
+            ], 'User Logged In', 200);
+        }
+
+
+    }
+
+    public function logout (Request $request) {
+        $token = $request->user()->currentAccessToken()->delete();
+        return $this->success([], 'User logged out', 200);
+    }
+
+    public function register(Request $request){
+
+        $rules = [
+            'name' => ['required'],
+            'f_lastname' => ['required'],
+            'm_lastname' => ['required'],
+            'email' => ['required', 'unique:users', 'email:rfc'],
+            'phone' => ['required', 'unique:users', 'min:9'],
+            'birthday' => ['required', 'date'],
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()->letters()->numbers()],
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()){
+            return $this->error(['errors' => $validator->errors()], 'Incorrect registration data', 422);
+        }
+
+        $data_array = [
+            'name' => $request->name,
+            'f_lastname' => $request->f_lastname,
+            'm_lastname' => $request->m_lastname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'birthday' => \Carbon\Carbon::createFromFormat('Y-m-d', $request->birthday),
+            'password' => Hash::make($request->password),
+            'remember_token' => Str::random(10)
+        ];
+
+        $user = User::create($data_array);
+        $token = $user->createToken('API Token ' . Str::random(10));
+        return $this->success([
+            'user' => $user,
+            'token' => $this->token($token->plainTextToken)
+        ], 'User successfully registered', 200);
+
+    }
+}
